ae52bbabc765d5a37a3bd8e1cc97070d2ad32500
Dimitar Dimitrov <daghostman.dd@gmail.com>
2020-03-25 22:01:37 +0200

    minimal steps to reproduce build failure

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FunctionalComponent, h } from "preact";
-import { Route, Router, RouterOnChangeArgs } from "preact-router";
+import { Router, RouterOnChangeArgs } from "preact-router";
+import Route from "preact-async-route";
 
-import Home from "../routes/home";
 import Profile from "../routes/profile";
-import NotFoundPage from '../routes/notfound';
+import NotFoundPage from "../routes/notfound";
 import Header from "./header";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -13,16 +14,20 @@ if ((module as any).hot) {
 }
 
 const App: FunctionalComponent = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
+    const handleRoute = (e: RouterOnChangeArgs): void => {
+        console.log(e.url);
     };
 
     return (
         <div id="app">
             <Header />
             <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
+                <Route
+                    path="/"
+                    getComponent={(): Promise<any> =>
+                        import("../routes/home").then(module => module.default)
+                    }
+                />
                 <Route path="/profile/" component={Profile} user="me" />
                 <Route path="/profile/:user" component={Profile} />
                 <NotFoundPage default />
